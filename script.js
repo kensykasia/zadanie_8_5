8534932a7680701b3e52387cfd7c9f4e076344f4
Katarzyna Kensy <zbierek@gmail.com>
2018-04-19 13:12:43 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(getTriangleArea(10,6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 14);
+var triangle3Area = getTriangleArea(5, 10);
+
+console.log(triangle3Area);
